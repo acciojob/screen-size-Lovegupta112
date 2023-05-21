1db252f9e0a41af7f70dd50f82da1d072b0d344d
lovegupta112 <loveguptaofficials@gmail.com>
2023-05-21 16:31:58 +0530

    add scren-size

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+let h1=document.createElement('h1');
+h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+
+// console.log(h1.innerText);
+document.body.append(h1);
+
+window.onresize=function(){
+    h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+};
