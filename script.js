4c2a36bbf1521ccfd0ac873bf2d8fdf9f08eacce
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-05-21 17:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //your JS code here. If required.
-
+let div=document.createElement('div');
+div.id="sizeInfo";
 let h1=document.createElement('h1');
 h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 
 // console.log(h1.innerText);
-document.body.append(h1);
+document.body.append(div);
+div.append(h1);
+
 
 window.onresize=function(){
     h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
